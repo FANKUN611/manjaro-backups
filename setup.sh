c8fb9706c2f0f54a0fc345ee7fa0a05b1803fd89
fankun <80640497@qq.com>
2017-06-19 16:09:06 +0800

    安装主题

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cp ~/manjaro-backups/vimrc /etc/
 cp ~/manjaro-backups/.oh-my-zsh/ ~
+unzip adapta-gtk-theme-master
+cd adapta-gtk-theme-master/
+./autogen.sh&& make&&sudo make install
 
